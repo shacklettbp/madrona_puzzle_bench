46305762d83343a60bf8db62ef943a07367bea99
Vishnu Sarukkai <vishnu.sarukkai@hotmail.com>
2024-03-12 14:56:37 -0700

    Script

---
+++ fixed_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for seed in 0 1 2 3 4 5 6 7
+do
+    CUDA_VISIBLE_DEVICES=$seed python scripts/go_ppo.py --num-worlds 8192 --fp16 --reward-mode Dense2 --run-name hello --num-steps 10000 --num-bins 1000 --num-checkpoints 1000 --seeds-per-checkpoint 512 --binning block_button --ckpt-dir ckpts/block_button_seed_$seed --num-updates 10000 --bin-reward-type none --bin-reward-boost 0.01 --num-bptt-chunks 1 --new-frac 0.95 --sampling-strategy uniform --gpu-sim --steps-per-update 40 --make-graph --use-fixed-world --seed $seed &
+done
